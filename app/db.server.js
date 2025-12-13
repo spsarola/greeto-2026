c03a14045358b83de918f3d72f8a800031676f75
sarolasp <spsarola777@gmail.com>
2025-12-13 13:48:08 +0530

    solution for Do not know how to serialize a BigInt

--- app/db.server.js
+++ app/db.server.js
@@ -1,5 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 
+// ✅ import this once — patches BigInt globally
+import "./utils/bigint-serializer.js";
+
 if (process.env.NODE_ENV !== "production") {
   if (!global.prismaGlobal) {
     global.prismaGlobal = new PrismaClient();
